6f64df9f0ef7195cc3b2949a9a03ff8626d1f66d
ManojSinghDashauni <97912816+ManojSinghDashauni@users.noreply.github.com>
2022-06-14 22:08:05 +0530

    college code

---
+++ 20 anil kumar gupta.c
@@ -0,0 +1,25 @@
+//NAME-MANOJ SINGH DASHAUNI
+//ROLL NO-12
+//COURSE-BCA
+//SECTION-F
+
+/*
+PROBLEM STATEMENT
+WAP to convert a name to a given format as if ANIL KUMAR GUPTA to A.K.Gupta
+*/
+
+#include<stdio.h>
+
+void main(){
+    char Str1[10]="ANIL";
+    char Str2[10]="KUMAR";
+    char Str3[10]="GUPTA";
+
+    printf("%.1s.%.1s.%s\n",Str1,Str2,Str3);
+}
+
+/*
+OUTPUT
+A.K.GUPTA
+*/  	
+
